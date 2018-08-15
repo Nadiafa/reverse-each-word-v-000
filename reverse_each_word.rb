c51c89f087e0323d6890cbee058ac85aeb611ed3
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-15 18:39:04 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
 def reverse_each_word(phrase)
   phrase.reverse.split.reverse.join(" ")
 end 
-sentence.split.collect {|word| word.reverse}.join(" ")
+# sentence.split.collect {|word| word.reverse}.join(" ")
