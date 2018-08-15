d819c2f42bbf576674be4747e67653f3bea088bb
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-15 18:11:12 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(phrase)
   phrase_array=[]
   phrase_array << phrase.split
-  
+  reversed_words_array = []
+  phrase_array {|word| reversed_words_array <<  word.reverse}
 
 end 
