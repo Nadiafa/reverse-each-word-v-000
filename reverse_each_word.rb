77536b295f191a8897007d4ac0ca4d75b51ce169
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-15 18:21:05 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,13 @@ def reverse_each_word(phrase)
   phrase_array.each {|word| reversed_words_array <<  word.reverse}
   reversed_words_array.join(" ")
 end 
+
+
+
+def reverse(phrase_array)
+reversed_phrase_array=[]
+phrase_array.each {|element| reversed_phrase_array << element.reverse}
+ puts reversed_phrase_array
+end
+=> :reverse
+irb(main):017:0> reverse(phrase)
