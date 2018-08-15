7294a701053c2a4acd35596dffcdcfc24aec7c7b
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-15 18:22:40 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,18 +2,9 @@ def reverse_each_word(phrase)
   phrase_array=[]
   phrase_array << phrase.split
   
-  
-  reversed_words_array = []
-  phrase_array.each {|word| reversed_words_array <<  word.reverse}
-  reversed_words_array.join(" ")
+  reversed_phrase_array=[]
+  phrase_array.each {|element| reversed_phrase_array << element.reverse}
+   puts reversed_phrase_array
 end 
 
 
-
-def reverse(phrase_array)
-reversed_phrase_array=[]
-phrase_array.each {|element| reversed_phrase_array << element.reverse}
- puts reversed_phrase_array
-end
-
-reverse(phrase_array)
